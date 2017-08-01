f5304266868936883dfc9e866b35581c2ae7c84f
LAPTOP-2QRIF7JH\Manas Gangwani <manasjg1969@gmail.com>
2017-08-01 17:48:27 +0530

    Ball control Protoype ready

---
+++ Assets/BallLaunch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLaunch : MonoBehaviour {
+    public Vector3 launchSpeed;
+    public float spin=0f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        GetComponent<Rigidbody>().AddTorque(transform.forward*spin );
+
+    }
+    public void ballLaunch()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().AddForce(launchSpeed);
+    }
+ 
+}
---
+++ Assets/Swing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour {
+    public float batspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().AddTorque(transform.right * batspeed);
+        Debug.Log(GetComponent<Rigidbody>().angularVelocity);
+    }
+}
