4c2b961e164b9b7b01c8116efa92011b4e17a730
LAPTOP-2QRIF7JH\Manas Gangwani <manasjg1969@gmail.com>
2017-08-02 17:58:41 +0530

    Ball Physics Complete Set Ready

---
+++ Assets/Scripts/BallLaunch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLaunch : MonoBehaviour {
+    public Vector3 launchSpeed;
+    public float spin=0f;
+    public int av;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().maxAngularVelocity = av;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().AddTorque(transform.forward*spin );
+        
+
+    }
+    public void ballLaunch()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().AddForce(launchSpeed);
+    }
+ 
+}
---
+++ Assets/Scripts/Swing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour {
+    public float batspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GetComponent<Rigidbody>().AddTorque(transform.right * batspeed);
+        //Debug.Log(GetComponent<Rigidbody>().angularVelocity);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.contacts[0].point);
+    }
+}
